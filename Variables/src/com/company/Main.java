876fd654ead09f290dacfb58421173d26994b1bb
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2021-05-10 12:45:21 +0200

    Variables

---
+++ Variables/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	What are variables?
+	A variable is a place holder or a pointer to an object.
+	A variable is a location in memory (storage area) to hold data.
+	To indicate the storage area, each variable should be given a unique name (identifier).
+
+    */
+        // Not initialized
+        int Number;
+        String Word;
+        char Character;
+        Boolean Question;
+        float realNumber;
+        double Digits;
+
+        // Initialized
+        Number =5;
+        Word = "kid";
+        Character = ' ';
+        Question = true;
+        realNumber = 32.5F;
+        Digits = 37.564748;
+
+
+
+    }
+}
