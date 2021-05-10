1bca4b063e0032f68151cdb8249f24fd9c233f01
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2021-05-10 16:10:40 +0200

    Expressions & Blocks

---
+++ ExpressionsAndBlocks/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	* We can convert an expression into a statement by terminating the expression with a ;.
+	* These are known as expression statements.
+	* A block is a group of statements (zero or more) that is enclosed in curly braces
+     * */
+        // expression
+       // number = 10
+        // statement
+        int number = 10;
+        String band="banana";
+        //block
+        if (band == "Beatles") { // start of block
+            System.out.print("Hey ");
+            System.out.print("Jude!");
+        }
+    }
+}
