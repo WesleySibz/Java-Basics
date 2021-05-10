ec6e35b178d14a66db886eea88c4489cf0b9b652
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2021-05-10 16:09:53 +0200

    Comments

---
+++ Comments/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	* In computer programming, comments are a portion of the program that are completely ignored by Java compilers.
+	* They are mainly used to help programmers to understand the code.
+	* One thing you should always consider that comments shouldn't be the substitute for a way to explain poorly written code in English.
+	* You should always write well structured and self explaining code.
+	* And, then use comments.
+	* */
+
+        // declare and initialize two variables
+        int a =1;
+        int b = 3;
+
+        // print the output
+        System.out.println("This is output");
+    }
+}
