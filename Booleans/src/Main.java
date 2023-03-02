3a17d67e743b96a2e9d3b1f2550ae6b66cd7acc3
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-03-02 12:55:02 +0200

    Boolean

---
+++ Booleans/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /**Boolean Expression
+         A Boolean expression returns a boolean value: true or false.
+
+         This is useful to build logic, and find answers.
+
+         For example, you can use a comparison operator,
+         such as the greater than (>) operator,
+         to find out if an expression (or a variable) is true or false:*/
+
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y); // returns true, because 10 is higher than 9
+        // or :
+        System.out.println(x > y); // returns true, because 10 is higher than 9
+        //In the examples below, we use the equal to (==) operator to evaluate an expression:
+        int z = 10;
+        System.out.println(z == 10); // returns true, because the value of z is equal to 10
+        /**In the example below,
+         * we use the >= comparison operator to find out if the age (25)
+         * is greater than OR equal to the voting age limit,
+         * which is set to 18:*/
+        int myAge = 25;
+        int votingAge = 18;
+        System.out.println(myAge >= votingAge);
+
+    }
+}
