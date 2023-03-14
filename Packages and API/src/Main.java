3976cf3f0698634b25e07a95bbeea5baa0e0aba2
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-03-14 09:22:13 +0200

    Packages & API's

---
+++ Packages and API/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    /**Java Packages & API
+     A package in Java is used to group related classes. Think of it as a folder in a file directory. We use packages to avoid name conflicts, and to write a better maintainable code. Packages are divided into two categories:
+
+     Built-in Packages (packages from the Java API)
+     User-defined Packages (create your own packages)
+     User-defined Packages
+     To create your own package, you need to understand that Java uses a file system directory to store them.
+     Just like folders on your computer:
+     package mypack;
+     class MyPackageClass {
+     public static void main(String[] args) {
+     System.out.println("This is my package!");
+     }
+     }*/
+    public static void main(String[] args) {
+
+
+    }
+}
